1edadb7607c7d064d209c2e2435a1fe3173f64b6
Erika <haloerika@gmail.com>
2018-04-16 10:57:57 +0900

    update settings file

--- config/settings/production.py
+++ config/settings/production.py
@@ -2,3 +2,7 @@ from .base import *
 
 
 DEBUG = False
+
+# Update database configuration with $DATABASE_URL.
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
